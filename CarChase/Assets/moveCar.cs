ac4f2bf0bfdf1d8663f61e2540dbcc1fd12d7049
AppleWhisky <gameboy4393@naver.com>
2015-06-01 22:21:51 +0900

    Unity CarChase Game
    posted for Testing

---
+++ CarChase/Assets/moveCar.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCar : MonoBehaviour {
+
+	int speed;
+	// Use this for initialization
+	void Start () {
+		speed = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += transform.forward * speed * Time.deltaTime;
+	}
+}
---
+++ CarChase/Assets/moveDog.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class moveDog : MonoBehaviour {
+
+	GameObject doge;
+
+	int speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5;
+		doge = GameObject.FindGameObjectWithTag ("Doge");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += transform.forward * speed * Time.deltaTime;
+
+		if (doge.transform.position.x < -1) {
+			// Nothing Happens
+		} else {
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				transform.Translate(Vector3.left*(2));
+			}
+		}
+
+		if (doge.transform.position.x > 1) {
+			// Nothing Happens
+		} else {
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				transform.Translate(Vector3.left*(-2));
+			}
+		}
+	}
+}
